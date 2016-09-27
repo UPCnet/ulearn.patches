06a88bf6f43337aef5587881a75bab02559d8b4f
Pilar Marinas <pilar.marinas@upcnet.es>
2016-09-27 15:49:44 +0200

    Paquete con los parches grupos con acento

--- ulearn/patches/patches.py
+++ ulearn/patches/patches.py
@@ -1,25 +1,7 @@
 import os
 import ldap
 from ldap.filter import filter_format
-
-ALT_LDAP_URI = os.environ.get('alt_ldap_uri', '')
-ALT_LDAP_DN = os.environ.get('alt_bind_dn', '')
-ALT_LDAP_PASSWORD = os.environ.get('alt_bindpasswd', '')
-BASEDN = os.environ.get('alt_base_dn', '')
-
-CA_GROUPS_QUERY = '(objectClass=groupOfNames)'
-CA_USER_GROUPS_QUERY = '(&(objectClass=groupOfNames)(member=%s))'
-
-
-def search_ldap_groups():
-    """
-        Patches ldap search on group search performed in syncaclgroups view
-        in order to inject an extra filter to limit results for Credit Andorra LDAP.
-    """
-    conn = ldap.initialize(ALT_LDAP_URI)
-    conn.simple_bind_s(ALT_LDAP_DN, ALT_LDAP_PASSWORD)
-    return conn.search_s(BASEDN, ldap.SCOPE_SUBTREE, CA_GROUPS_QUERY, ['cn'])
-
+from genweb.core.directory.views import get_ldap_config
 
 def from_latin1(s):
     """
@@ -29,12 +11,29 @@ def from_latin1(s):
     return s.decode('utf-8')
 
 
+from plone.memoize.instance import clearafter
+from plone.app.workflow.browser.sharing import SharingView
+
+original_update_role_settings = SharingView.update_role_settings
+
+
+@clearafter
+def update_role_settings(self, new_settings, reindex=True):
+    def fix_encoding(entry):
+        if not isinstance(entry['id'], unicode):
+            entry['id'] = entry['id'].decode('utf-8')
+        return entry
+    settings = [fix_encoding(entry) for entry in new_settings]
+    original_update_role_settings(self, settings)
+
+
 def getGroups(self, dn='*', attr=None, pwd=''):
     """
         returns a list of possible groups from the ldap tree
         (Used e.g. in showgroups.dtml) or, if a DN is passed
         in, all groups for that particular DN.
     """
+    ALT_LDAP_URI, ALT_LDAP_DN, ALT_LDAP_PASSWORD, BASEDN, GROUPS_QUERY, USER_GROUPS_QUERY = get_ldap_config()
     group_list = []
     no_show = ('Anonymous', 'Authenticated', 'Shared')
     if self._local_groups:
@@ -63,9 +62,9 @@ def getGroups(self, dn='*', attr=None, pwd=''):
         gscope = self._delegate.getScopes()[self.groups_scope]
 
         if dn != '*':
-            group_filter = filter_format(CA_USER_GROUPS_QUERY, (dn,))
+            group_filter = filter_format(USER_GROUPS_QUERY, (dn,))
         else:
-            group_filter = CA_GROUPS_QUERY
+            group_filter = GROUPS_QUERY
 
         res = self._delegate.search(
             base=self.groups_base,
@@ -113,6 +112,7 @@ def searchGroups(self, attrs=(), exact_match=False, **kw):
     is currently no schema support for groups. The list of accepted
     group attributes is static for now.
     """
+    ALT_LDAP_URI, ALT_LDAP_DN, ALT_LDAP_PASSWORD, BASEDN, GROUPS_QUERY, USER_GROUPS_QUERY = get_ldap_config()
     groups = []
     groups_base = self.groups_base
     filt_list = []
@@ -163,7 +163,7 @@ def searchGroups(self, attrs=(), exact_match=False, **kw):
         res['size'] = 0
 
     else:
-        oc_filt = CA_GROUPS_QUERY
+        oc_filt = GROUPS_QUERY
         filt_list.append(oc_filt)
         search_str = '(&%s)' % ''.join(filt_list)
         res = self._delegate.search(
@@ -239,22 +239,6 @@ def fixed_make_query(*args, **kwargs):
     return '&'.join(qlist)
 
 
-from plone.memoize.instance import clearafter
-from plone.app.workflow.browser.sharing import SharingView
-
-original_update_role_settings = SharingView.update_role_settings
-
-
-@clearafter
-def update_role_settings(self, new_settings, reindex=True):
-    def fix_encoding(entry):
-        if not isinstance(entry['id'], unicode):
-            entry['id'] = entry['id'].decode('utf-8')
-        return entry
-    settings = [fix_encoding(entry) for entry in new_settings]
-    original_update_role_settings(self, settings)
-
-
 import unicodedata
 _marker = []
 from BTrees.IIBTree import IITreeSet
@@ -287,3 +271,156 @@ def insertForwardIndexEntry(self, entry, documentId):
             # first (before Zope 2.13).
             indexRow = IITreeSet((indexRow, documentId))
             self._index[entry] = indexRow
+
+
+from ZODB.POSException import ConflictError
+from BTrees.Length import Length
+from logging import getLogger
+LOG = getLogger('Zope.UnIndex')
+import sys
+
+
+def removeForwardIndexEntry(self, entry, documentId):
+        """Take the entry provided and remove any reference to documentId
+        in its entry in the index.
+        """
+        try:
+            indexRow = self._index.get(entry, _marker)
+        except:
+            entry = unicodedata.normalize('NFKD', entry.decode('utf-8')).encode('ascii', errors='ignore')
+            indexRow = self._index.get(entry, _marker)
+
+        if indexRow is not _marker:
+            try:
+                indexRow.remove(documentId)
+                if not indexRow:
+                    del self._index[entry]
+                    self._length.change(-1)
+
+            except ConflictError:
+                raise
+
+            except AttributeError:
+                # index row is an int
+                try:
+                    del self._index[entry]
+                except KeyError:
+                    # XXX swallow KeyError because it was probably
+                    # removed and then _length AttributeError raised
+                    pass
+                if isinstance(self.__len__, Length):
+                    self._length = self.__len__
+                    del self.__len__
+                self._length.change(-1)
+
+            except:
+                LOG.error('%s: unindex_object could not remove '
+                          'documentId %s from index %s.  This '
+                          'should not happen.' % (self.__class__.__name__, str(documentId), str(self.id)),
+                          exc_info=sys.exc_info())
+        else:
+            LOG.error('%s: unindex_object tried to retrieve set %s '
+                      'from index %s but couldn\'t.  This '
+                      'should not happen.' % (self.__class__.__name__, repr(entry), str(self.id)))
+
+
+from Acquisition import aq_parent, aq_inner, aq_get
+from Products.PlonePAS.utils import getGroupsForPrincipal
+
+def getRolesForPrincipal(self, principal, request=None):
+        """ See IRolesPlugin.
+        """
+        roles = set([])
+        principal_ids = set([])
+        # Some services need to determine the roles obtained from groups
+        # while excluding the directly assigned roles.  In this case
+        # '__ignore_direct_roles__' = True should be pushed in the request.
+        request = aq_get(self, 'REQUEST', None)
+        if request is None or \
+            not request.get('__ignore_direct_roles__', False):
+            principal_ids.add(principal.getId())
+
+        # Some services may need the real roles of an user but **not**
+        # the ones he got through his groups. In this case, the
+        # '__ignore_group_roles__'= True should be previously pushed
+        # in the request.
+        plugins = self._getPAS()['plugins']
+        if request is None or \
+            not request.get('__ignore_group_roles__', False):
+            principal_ids.update(getGroupsForPrincipal(principal, plugins,
+                                                       request))
+        for pid in principal_ids:
+            roles.update(self._principal_roles.get(pid.encode('utf-8'), ()))
+        return tuple(roles)
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import normalizeString
+
+def update(self):
+        self.groupname = getattr(self.request, 'groupname')
+        self.gtool = getToolByName(self, 'portal_groups')
+        self.mtool = getToolByName(self, 'portal_membership')
+        self.group = self.gtool.getGroupById(self.groupname.decode('utf-8'))
+        self.grouptitle = self.group.getGroupTitleOrName() or self.groupname
+
+        self.request.set('grouproles', self.group.getRoles() if self.group else [])
+        self.canAddUsers = True
+        if 'Manager' in self.request.get('grouproles') and not self.is_zope_manager:
+            self.canAddUsers = False
+
+        self.groupquery = self.makeQuery(groupname=self.groupname)
+        self.groupkeyquery = self.makeQuery(key=self.groupname)
+
+        form = self.request.form
+        submitted = form.get('form.submitted', False)
+
+        self.searchResults = []
+        self.searchString = ''
+        self.newSearch = False
+
+        if submitted:
+            # add/delete before we search so we don't show stale results
+            toAdd = form.get('add', [])
+            if toAdd:
+                if not self.canAddUsers:
+                    raise Forbidden
+
+                for u in toAdd:
+                    self.gtool.addPrincipalToGroup(u, self.groupname, self.request)
+                self.context.plone_utils.addPortalMessage(_(u'Changes made.'))
+
+            toDelete = form.get('delete', [])
+            if toDelete:
+                for u in toDelete:
+                    self.gtool.removePrincipalFromGroup(u, self.groupname, self.request)
+                self.context.plone_utils.addPortalMessage(_(u'Changes made.'))
+
+            search = form.get('form.button.Search', None) is not None
+            edit = form.get('form.button.Edit', None) is not None and toDelete
+            add = form.get('form.button.Add', None) is not None and toAdd
+            findAll = form.get('form.button.FindAll', None) is not None and \
+                not self.many_users
+            # The search string should be cleared when one of the
+            # non-search buttons has been clicked.
+            if findAll or edit or add:
+                form['searchstring'] = ''
+            self.searchString = form.get('searchstring', '')
+            if findAll or bool(self.searchString):
+                self.searchResults = self.getPotentialMembers(self.searchString)
+
+            if search or findAll:
+                self.newSearch = True
+
+        self.groupMembers = self.getMembers()
+
+def getMembers(self):
+        searchResults = self.gtool.getGroupMembers(self.groupname.decode('utf-8'))
+
+        groupResults = [self.gtool.getGroupById(m) for m in searchResults]
+        groupResults.sort(key=lambda x: x is not None and normalizeString(x.getGroupTitleOrName()))
+
+        userResults = [self.mtool.getMemberById(m) for m in searchResults]
+        userResults.sort(key=lambda x: x is not None and x.getProperty('fullname') is not None and normalizeString(x.getProperty('fullname')) or '')
+
+        mergedResults = groupResults + userResults
+        return filter(None, mergedResults)
